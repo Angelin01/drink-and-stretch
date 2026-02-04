e82a408eebe7a9728f194f105da211ec360a01d0
Angelin01 <angeloborsoiross@hotmail.com>
2026-02-03 23:21:42 -0300

    Show toast on world join in neoforge

--- neoforge/src/main/java/com/angelin01/drinkandstretch/neoforge/DrinkAndStretchNeoForge.java
+++ neoforge/src/main/java/com/angelin01/drinkandstretch/neoforge/DrinkAndStretchNeoForge.java
@@ -1,13 +1,18 @@
 package com.angelin01.drinkandstretch.neoforge;
 
-import net.neoforged.fml.common.Mod;
-
 import com.angelin01.drinkandstretch.DrinkAndStretch;
+import com.angelin01.drinkandstretch.neoforge.events.EventHandler;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(DrinkAndStretch.MOD_ID)
 public final class DrinkAndStretchNeoForge {
-	public DrinkAndStretchNeoForge() {
+	public DrinkAndStretchNeoForge(IEventBus modBus) {
 		DrinkAndStretch.init();
 		System.out.println("Hello from neoforge!");
+
+		NeoForge.EVENT_BUS.register(new EventHandler());
 	}
 }
