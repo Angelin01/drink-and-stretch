62fcbd173f5c99ea59c84f95bd454fbbdabe78ad
Angelin01 <angeloborsoiross@hotmail.com>
2026-02-12 22:16:56 -0300

    Add combat events for neoforge

--- neoforge/src/main/java/com/angelin01/drinkandstretch/neoforge/events/EventHandler.java
+++ neoforge/src/main/java/com/angelin01/drinkandstretch/neoforge/events/EventHandler.java
@@ -1,9 +1,10 @@
 package com.angelin01.drinkandstretch.neoforge.events;
 
 import com.angelin01.drinkandstretch.DrinkAndStretch;
-
+import net.minecraft.client.player.LocalPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
+import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 
 public class EventHandler {
 	@SubscribeEvent
@@ -14,5 +15,12 @@ public class EventHandler {
 	@SubscribeEvent
 	public void onClientLogout(ClientPlayerNetworkEvent.LoggingOut event) {
 		DrinkAndStretch.stopPeriodicReminders();
+	}
+
+	@SubscribeEvent
+	public void onPlayerAttack(AttackEntityEvent event) {
+		if (event.getEntity() instanceof LocalPlayer player) {
+			DrinkAndStretch.onPlayerAttack(player, event.getTarget());
+		}
 	}
 }
---
+++ neoforge/src/main/java/com/angelin01/drinkandstretch/neoforge/mixins/LocalPlayerMixin.java
@@ -0,0 +1,19 @@
+package com.angelin01.drinkandstretch.neoforge.mixins;
+
+import com.angelin01.drinkandstretch.DrinkAndStretch;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LocalPlayer.class)
+public class LocalPlayerMixin {
+	@Inject(method = "hurt", at = @At("HEAD"))
+	private void onHurt(DamageSource damageSource, float _amount, CallbackInfoReturnable<Boolean> _cir) {
+		final LocalPlayer player = (LocalPlayer) (Object) this;
+		DrinkAndStretch.onPlayerReceiveDamage(player, damageSource);
+	}
+}
