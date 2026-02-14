368d68b8da29e0ff44cb42660cb31bbf83ad79a1
Angelin01 <angeloborsoiross@hotmail.com>
2026-02-14 11:01:45 -0300

    Merge mixins into common mod

--- common/src/main/java/com/angelin01/drinkandstretch/mixins/LocalPlayerMixin.java
+++ common/src/main/java/com/angelin01/drinkandstretch/mixins/LocalPlayerMixin.java
@@ -1,4 +1,4 @@
-package com.angelin01.drinkandstretch.neoforge.mixins;
+package com.angelin01.drinkandstretch.mixins;
 
 import com.angelin01.drinkandstretch.DrinkAndStretch;
 import net.minecraft.client.player.LocalPlayer;
@@ -6,7 +6,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LocalPlayer.class)
