e1c916a8aacd7549b8a7a82ea0b33f035f09586e
Angelin01 <angeloborsoiross@hotmail.com>
2026-02-14 00:02:07 -0300

    Defer toast reminders when in combat
    This is done on a best effort basis, since the
    client doesn't get all events, and often gets
    multiple events with specific damage sources
    followed by generic sources that we can't
    identify. For some reason, we can identify
    cacti damage.

--- common/src/main/java/com/angelin01/drinkandstretch/reminders/ReminderScheduler.java
+++ common/src/main/java/com/angelin01/drinkandstretch/reminders/ReminderScheduler.java
@@ -1,12 +1,14 @@
 package com.angelin01.drinkandstretch.reminders;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -14,20 +16,43 @@ import com.mojang.logging.LogUtils;
 public final class ReminderScheduler implements AutoCloseable {
 	private static final Logger LOGGER = LogUtils.getLogger();
 
-	private final ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
+	private final ScheduledExecutorService executor;
 	private final List<ScheduledFuture<?>> reminders = new ArrayList<>();
 
-	public void schedule(int periodMin, Runnable task) {
-		ReminderScheduler.LOGGER.info(
-			"Scheduling reminder: task={}, period={}min",
-			task,
-			periodMin
-		);
+	public ReminderScheduler(ScheduledExecutorService executor) {
+		this.executor = executor;
+	}
 
-		// TODO: change TimeUnit.Seconds to Minutes later
-		final ScheduledFuture<?> future = this.timer.scheduleAtFixedRate(task, periodMin, periodMin, TimeUnit.SECONDS);
+	public void schedule(Duration period, Duration deferDuration, Supplier<@NotNull Boolean> shouldDefer, Runnable task) {
+		ReminderScheduler.LOGGER.info("Scheduling reminder: period={}min", period.toMinutes());
+		this.scheduleNext(period, period, deferDuration, shouldDefer, task);
+	}
 
+	private void scheduleNext(Duration delay, Duration period, Duration deferDuration, Supplier<Boolean> shouldDefer, Runnable task) {
+		final Runnable wrappedTask = this.wrapTask(period, deferDuration, shouldDefer, task);
+
+		ScheduledFuture<?> future = this.executor.schedule(wrappedTask, delay.toSeconds(), TimeUnit.SECONDS);
 		this.reminders.add(future);
+	}
+
+	private @NotNull Runnable wrapTask(Duration period, Duration deferDuration, Supplier<Boolean> shouldDefer, Runnable task) {
+		return () -> {
+			Duration nextDelay;
+
+			if (shouldDefer.get()) {
+				ReminderScheduler.LOGGER.info("Task deferred, rescheduling in {}s", deferDuration.toSeconds());
+				nextDelay = deferDuration;
+			} else {
+				try {
+					task.run();
+				} catch (Exception e) {
+					ReminderScheduler.LOGGER.error("Error executing reminder task", e);
+				}
+				nextDelay = period;
+			}
+
+			this.scheduleNext(nextDelay, period, deferDuration, shouldDefer, task);
+		};
 	}
 
 	public void cancelAll() {
@@ -43,7 +68,7 @@ public final class ReminderScheduler implements AutoCloseable {
 	@Override
 	public void close() {
 		this.cancelAll();
-		this.timer.shutdownNow();
+		this.executor.shutdownNow();
 	}
 }
 
--- common/src/main/java/com/angelin01/drinkandstretch/reminders/ReminderService.java
+++ common/src/main/java/com/angelin01/drinkandstretch/reminders/ReminderService.java
@@ -6,48 +6,43 @@ import com.angelin01.drinkandstretch.toasts.ToastDispatcher;
 import com.angelin01.drinkandstretch.toasts.ToastText;
 import com.angelin01.drinkandstretch.toasts.ToastVariants;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ReminderService {
+	private static final Duration DEFER_DURATION = Duration.ofSeconds(15);
+
 	private final ReminderScheduler scheduler;
+	private final CombatTracker combatTracker;
 	private boolean running;
 
-	public ReminderService(ReminderScheduler scheduler) {
+	public ReminderService(ReminderScheduler scheduler, CombatTracker combatTracker) {
 		this.scheduler = scheduler;
+		this.combatTracker = combatTracker;
 	}
 
 	public void startOrRestart(DrinkAndStretchConfig config) {
 		this.scheduler.cancelAll();
 
 		if (config.isDrinkReminderEnabled()) {
-			List<ToastText> drinkMessages = config.isInsultsEnabled()
-				? Stream.concat(ToastVariants.DRINK.stream(), ToastVariants.DRINK_INSULTS.stream())
-				.collect(Collectors.toList())
-				: ToastVariants.DRINK;
-
-			this.scheduler.schedule(
+			this.scheduleReminder(
+				DrinkAndStretchToast.DrinkAndStretchToastId.DRINK,
 				config.getDrinkReminderInterval(),
-				ToastDispatcher.show(
-					DrinkAndStretchToast.DrinkAndStretchToastId.DRINK,
-					drinkMessages
-				)
+				ToastVariants.DRINK,
+				ToastVariants.DRINK_INSULTS,
+				config.isInsultsEnabled()
 			);
 		}
 
 		if (config.isStretchReminderEnabled()) {
-			List<ToastText> stretchMessages = config.isInsultsEnabled()
-				? Stream.concat(ToastVariants.STRETCH.stream(), ToastVariants.STRETCH_INSULTS.stream())
-				.collect(Collectors.toList())
-				: ToastVariants.STRETCH;
-
-			this.scheduler.schedule(
+			this.scheduleReminder(
+				DrinkAndStretchToast.DrinkAndStretchToastId.STRETCH,
 				config.getStretchReminderInterval(),
-				ToastDispatcher.show(
-					DrinkAndStretchToast.DrinkAndStretchToastId.STRETCH,
-					stretchMessages
-				)
+				ToastVariants.STRETCH,
+				ToastVariants.STRETCH_INSULTS,
+				config.isInsultsEnabled()
 			);
 		}
 
@@ -63,5 +58,26 @@ public class ReminderService {
 		if (this.running) {
 			this.startOrRestart(config);
 		}
+	}
+
+	private void scheduleReminder(
+		DrinkAndStretchToast.DrinkAndStretchToastId toastId,
+		int intervalMinutes,
+		List<ToastText> baseMessages,
+		List<ToastText> insultMessages,
+		boolean includeInsults
+	) {
+		List<ToastText> messages = includeInsults
+			? Stream.concat(baseMessages.stream(), insultMessages.stream())
+			.collect(Collectors.toList())
+			: baseMessages;
+
+		// TODO: change ofSeconds to ofMinutes
+		this.scheduler.schedule(
+			Duration.ofSeconds(intervalMinutes),
+			ReminderService.DEFER_DURATION,
+			this.combatTracker::wasRecentlyInCombat,
+			ToastDispatcher.show(toastId, messages)
+		);
 	}
 }
